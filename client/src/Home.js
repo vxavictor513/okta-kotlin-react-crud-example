f81a14040207c784758562746c6f2f4968bc751a
Andrew Hughes <andrewcarterhughes@gmail.com>
2019-12-03 11:00:26 -0800

    initial

---
+++ client/src/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'reactstrap';
+
+class Home extends Component {
+
+    render() {
+
+        if (this.props.authenticated === null) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div className="app">
+                {this.props.navbar}
+                <Container fluid>
+                    { this.props.authenticated ?
+                        <div>
+                            <p>Welcome, {this.props.user.name}</p>
+                            <Button color="secondary">
+                                <Link className="app-link" to="/coffee-shops">Manage Coffee Shops</Link>
+                            </Button>
+                        </div> :
+                        <div>
+                            <p>Please log in to manage coffee shops.</p>
+                            <Button color="secondary" disabled={true}>
+                                Manage Coffee Shops
+                            </Button>
+                        </div>
+                    }
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ client/src/NavBar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Button, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {isOpen: false};
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+
+        const {isAuthenticated, login, logout} = this.props;
+
+        return <Navbar color="light" light expand="md">
+            <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}/>
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink
+                            href="https://twitter.com/oktadev">@oktadev</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://github.com/oktadeveloper/okta-spring-boot-react-crud-example">GitHub</NavLink>
+                    </NavItem>
+                    { !isAuthenticated ?
+                        <NavItem>
+                            <Button color="secondary" outline onClick={login}>Login</Button>
+                        </NavItem> :
+                        <NavItem>
+                            <Button color="secondary" outline onClick={logout}>Logout</Button>
+                        </NavItem>
+                    }
+                </Nav>
+            </Collapse>
+        </Navbar>;
+    }
+}
+
+export default NavBar;
