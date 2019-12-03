0954738acc49cd7b5f778b24eec90b05cebcf87a
Andrew Hughes <andrewcarterhughes@gmail.com>
2019-12-03 11:06:23 -0800

    added resource server

---
+++ resourceserver/src/main/kotlin/com/okta/kotlinreact/SecurityConfiguration.kt
@@ -0,0 +1,20 @@
+package com.okta.kotlinreact
+
+import com.okta.spring.boot.oauth.Okta
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http
+            .csrf().disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer().jwt();
+
+        // Send a 401 message to the browser (w/o this, you'll see a blank page)
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
---
+++ resourceserver/src/test/kotlin/com/okta/kotlinreact/ResourceServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.okta.kotlinreact
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
